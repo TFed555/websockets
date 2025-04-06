02d380b02d241e51563c99f4cd3945cb7e809def
TFed555 <thedgeofwave@gmail.com>
2025-04-06 17:46:22 +0700

    first

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from 'express'
+import fs from 'fs'
+
+
+const port = 4000;
+const host = 'localhost';
+const app = express();
+
+app.use(express.static(process.cwd() + '/public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(process.cwd() + '/public/main.html')
+});
+
+app.listen(port, host, () =>{
+    console.log(`Listening on ${host}:${port}`)
+});
