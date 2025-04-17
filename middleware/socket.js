b244ce95b59a209d961580331ed08318a7b751a7
TFed555 <thedgeofwave@gmail.com>
2025-04-17 17:40:25 +0700

    Исправлено отображение цвета

--- middleware/socket.js
+++ middleware/socket.js
@@ -1,6 +1,5 @@
 //серверная сторона вебсокета ws
 
-// const fs = require('fs');
 import { WebSocketServer } from "ws";
 
 const wss = new WebSocketServer({port: 3000});
@@ -11,42 +10,23 @@ let listCoords = [];
 function onConnect(client) {
     console.log("Connection opened");
 
-  //   wss.clients.forEach( client => {
-  //   client.send(listCoords.length);
-  //   for (let i=0; i < listCoords.length;i+=1) {
-  //       client.send(listCoords[i]);
-  //   }
-  // })
-
-    // client.send(listCoords.length);
-    //   // client.send(listCoords);
-    // for (let i=0; i < listCoords.length;i+=1) {
-    //     client.send(listCoords[i]);
-    // }
-    // console.log('OnConnect event:', listCoords);
-
-
     client.on("message", (message) => {
       const data = JSON.parse(message);
-      console.log(data);
+      // console.log(data);
       if (data.type == 'Open') {
-          client.send(JSON.stringify({list_:listCoords, type: "connect"}));
-      //   client.send(listCoords.length);
-      //   for (let i=0; i < listCoords.length;i+=1) {
-      //     client.send(listCoords[i]);
-      // }
-    }
+        client.send(JSON.stringify({list_:listCoords, type: "connect"}));
+      }
 
-    else {
-      listCoords.push(message.toString());
-      wss.clients.forEach( _client => {
-        // console.log(strClient, message.toString());
-        // console.log(message.toString());
-        // console.log(listCoords);
-        // console.log(listCoords);
-        if(_client != client && _client.readyState === _client.OPEN){
-          _client.send(JSON.stringify({object:data, type:"draw"}));
-        }
+      else if (data.type == 'Color') {
+        client.send(JSON.stringify({color:data.color, type:"color"}));
+      }
+
+      else {
+        listCoords.push(message.toString());
+        wss.clients.forEach( _client => {
+          if(_client != client && _client.readyState === _client.OPEN){
+            _client.send(JSON.stringify({object:data, type: "draw"}));
+          }
     });
     }
       });
@@ -59,8 +39,5 @@ function onConnect(client) {
         console.error("WebSocket error:", error);
       });
 }
-// wss.on("close", () => {
-//   console.log("Server: Connection closed");
-// });
 
 console.log("Сервер запущен на 3000 порту");
--- public/socket.js
+++ public/socket.js
@@ -4,10 +4,12 @@ let url = "ws://localhost:3000"
 let ws = new WebSocket(url);
 
 const c = document.querySelector("#canvas");
+let picker = document.querySelector("input[type=color]");
+
 
 let ctx = c.getContext("2d");
-let log = console.log();
 let flag = false;
+let currentColor = picker.value;
 
 let line = []
 
@@ -15,38 +17,33 @@ const open = {
   type: 'Open'
 }
 
+
 function getCursorPos(canvas, event) {
   const rect = canvas.getBoundingClientRect();
   const x = event.clientX - rect.left;
   const y = event.clientY - rect.top;
-  // console.log(x, y);
   return {x, y}
 }
 
 ws.onopen = function(event) {
   console.log('Connection opened');
-
   ws.send(JSON.stringify(open));
-  // let len = event.data;
-  // console.log('EVENT ONOPEN', len);
-  // for (let i=0; i < len; i+=1) {
-  //   line.push(event.data);
-  // }
-  // paint();
 };
 
 ws.onmessage = function(event) {
   console.log("Server response:", JSON.parse(event.data));
-  // let data = event.data;
   let data = JSON.parse(event.data);
   if (data.type == "connect") {
     flag = true;
     for (let i = 0; i < data.list_.length; i += 1) {
       line.push(JSON.parse(data.list_[i]));
     }
-    console.log(line);
     paint();
     flag = false;
+  }
+  else if (data.type == "color") {
+    currentColor = data.color;
+    paint();
   }
   else {
     flag = true;
@@ -56,37 +53,14 @@ ws.onmessage = function(event) {
     flag = false;
   }
 
-
-  // let len = event.data;
-  // console.log('EVENT ONOPEN', len);
-  // for (let i=0; i < len; i+=1) {
-  //   line.push(event.data);
-  // }
-
-
-  // paint();
-
-  // line.push(event.data);
-  // console.log(event.data);
-  // // console.log("Value of line:", line);
-  // paint();
-  // if (line.length > 1) {
-  //   paint()
-  // }
 };
 
 function paint() {
-  ctx.strokeStyle = "black";
+  console.log(currentColor);
   ctx.lineWidth = 10;
-  // console.log('Paint() line:',line.length);
   for (i=0; i < line.length; i+=1) {
     let coordb = line[i];
-    // let coordb = JSON.parse(line[i]);
-    // let coorde = JSON.parse(line[i+1]);
-
-    // console.log(coordb);
-    // console.log(coorde);
-
+    ctx.strokeStyle = coordb.color;
     if (coordb.par == 'down') {
       ctx.beginPath();
       ctx.moveTo(coordb.x, coordb.y);
@@ -94,40 +68,39 @@ function paint() {
     else if (coordb.par == 'move' && flag == true) {
       ctx.lineTo(coordb.x, coordb.y);
       ctx.stroke();
-      ctx.beginPath();
-      ctx.moveTo(coordb.x, coordb.y);
     }
     else {
       ctx.moveTo(coordb.x, coordb.y);
       ctx.beginPath();
     }
-    // ctx.moveTo(coordb.x, coordb.y);
-    // ctx.lineTo(coorde.x, coorde.y);
   }
-
-  // line.length = 0;
-  // console.log(line);
-
 };
 
 ws.onerror = function(error) {
   console.error('WebSocket error:', error);
 };
 
+picker.addEventListener("input", function() {
+  currentColor = picker.value;
+  ws.send(JSON.stringify({type: 'Color', color: picker.value}));
+});
+
 c.addEventListener( 'mousedown', function(e)
 {
   const pos = getCursorPos(c, e);
   flag = true;
+  ctx.strokeStyle = currentColor;
   ctx.beginPath();
   ctx.moveTo(pos.x, pos.y);
-  ws.send(JSON.stringify({x: pos.x, y: pos.y, par: 'down'}));
+  ws.send(JSON.stringify({x: pos.x, y: pos.y, par: 'down', color: currentColor}));
 });
 
 c.addEventListener( 'mouseup', function(e) {
   flag = false;
+  ctx.strokeStyle = currentColor;
   ctx.beginPath();
   const pos = getCursorPos(c, e);
-  ws.send(JSON.stringify({x: pos.x, y: pos.y, par: 'up'}));
+  ws.send(JSON.stringify({x: pos.x, y: pos.y, par: 'up', color: currentColor}));
 });
 
 c.addEventListener( 'mousemove', function(e) {
@@ -135,10 +108,9 @@ c.addEventListener( 'mousemove', function(e) {
   if (flag) {
     ctx.lineTo(pos.x, pos.y);
     ctx.stroke();
-    ws.send(JSON.stringify({x: pos.x, y: pos.y, par: 'move'}));
+    ws.send(JSON.stringify({x: pos.x, y: pos.y, par: 'move', color: currentColor}));
   }
 });
-
 
 ws.onclose = event => {
   console.log('Connection closed');
